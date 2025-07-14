9766f67311dc267aed8139a57eea76193db2e174
zeus <zeushqp@gmail.com>
2025-07-14 18:15:37 -0500

    cambios en los metodos y mejoras en las respuesta de la API

--- src/main/java/com/std/ec/controller/ClienteController.java
+++ src/main/java/com/std/ec/controller/ClienteController.java
@@ -3,7 +3,13 @@ package com.std.ec.controller;
 import com.std.ec.model.entity.Cliente;
 import com.std.ec.service.ICliente;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -13,23 +19,37 @@ public class ClienteController {
     private ICliente clienteService;
 
     @PostMapping("cliente")
+    @ResponseStatus(HttpStatus.CREATED)
     public Cliente create(@RequestBody Cliente cliente){
         return clienteService.save(cliente);
     }
 
+
     @PutMapping("cliente")
+    @ResponseStatus(HttpStatus.CREATED)
     public Cliente update(@RequestBody Cliente cliente){
         return clienteService.save(cliente);
     }
 
     @DeleteMapping("cliente/{id}")
-    public void delete(@PathVariable Integer id){
-        Cliente clienteDelete=clienteService.findById(id);
+    public ResponseEntity <?> delete(@PathVariable Integer id){
+        Map<String,Object> response = new HashMap<>();
+
+        try {
+            Cliente clienteDelete=clienteService.findById(id);
             clienteService.delete(clienteDelete);
+            return  new ResponseEntity<>(clienteDelete,HttpStatus.NO_CONTENT);
+        }catch (DataAccessException exDT){
+            response.put("mensaje","Error al procesar la solicitud: "+exDT.getMessage());
+            response.put("cliente",null);
+
+            return  new ResponseEntity<>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 
     @GetMapping("cliente/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public Cliente showById(@PathVariable Integer id){
         return clienteService.findById(id);
     }
